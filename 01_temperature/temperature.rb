c1d7dd8a766478164c835db8f3859b0e6aa36801
Sandy Lai <sandywslai@gmail.com>
2015-04-14 16:56:44 -0400

    Refactored code and adjusted one od the body temperature test to have the right value

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,21 +1,7 @@
 def ftoc(f)
-  if f == 32
-    0
-  elsif f == 212
-    100
-  elsif f == 37
-    98.6
-  else
-    20
-  end
+  (f.to_f - 32) * 5/9.round(1)
 end
 
 def ctof(c)
-  if c == 0
-    32
-  elsif c == 100
-    212
-  else
-    68
-  end
+  c.to_f * 9/5 + 32.round(1)
 end
