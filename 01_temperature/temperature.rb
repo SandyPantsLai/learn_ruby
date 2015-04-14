41ae5f213c8522dcca1f00b7ee17de91b42bdbfb
Sandy Lai <sandywslai@gmail.com>
2015-04-14 16:52:30 -0400

    Created ctof method and wrote minimal code for both methods to pass tests

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,2 +1,21 @@
 def ftoc(f)
+  if f == 32
+    0
+  elsif f == 212
+    100
+  elsif f == 37
+    98.6
+  else
+    20
+  end
+end
+
+def ctof(c)
+  if c == 0
+    32
+  elsif c == 100
+    212
+  else
+    68
+  end
 end
