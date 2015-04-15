c600f5b5268ffb2a834470bc5ca3fc8d88b6c4a2
Sandy Lai <sandywslai@gmail.com>
2015-04-14 23:23:33 -0400

    Passed all pig latin tests

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -3,13 +3,17 @@ def translate(phrase)
   vowels = ["a", "e", "i", "o", "u"]
   consonants = letters - vowels
   pig_latin = phrase.gsub(/\w+/) do|word| #regexp for word character one or more times in a row
-    if vowels.include?(word.downcase[0])
+    word.downcase!
+    idx = word.index(/[aeiou]/)
+    if idx == word[0]
       word + "ay"
+    elsif word[idx-1] == "q"
+      word[idx+1..word.length-idx+1] + word[0..idx] + "ay"
     else
-      idx = word.index(/[aeiou]/) #return index match for first a or e or etc.
       word[idx, word.length-idx] + word[0,idx] + "ay"
     end
   end
 end
+
 
 
